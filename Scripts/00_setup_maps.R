8864bdfc5231590af1c2ac3ad4183a111dec9887
tim <tim.essam@gmail.com>
2020-06-12 15:26:23 -0400

    prototype for mapping

--- Scripts/00_setup_maps.R
+++ Scripts/00_setup_maps.R
@@ -1,12 +1,14 @@
 # Purpose: Setup basemap and geographic matches to plot Zambia data
 # Author: Tim Essam | SI Asc
 # Date: 2020_06_10
-
+# NOTES: For inner glow?! https://www.katiejolly.io/blog/2020-03-06/inner-glow
 
 # PRELIMS -----------------------------------------------------------------
 
 library(rnaturalearthdata)
+library(tidyverse)
 library(raster)
+library(rgeos)
 library(sf)
 library(rnaturalearth)
 library(rnaturalearthhires)
@@ -14,65 +16,129 @@ library(extrafont)
 library(ggspatial)
 library(ggtext)
 library(ggsflabel)
+library(glitr)
+library(glamr)
+library(googledrive)
+library(here)
 
 
 # GLOBALS -----------------------------------------------------------------
 
+  dem_url <- https://drive.google.com/drive/u/0/folders/1M02ToX9AnkozOHtooxU7s4tCnOZBTvm_
 
   data <- "Data"
   data_out <- "Dataout"
   images <- "Images"
   gis <- "GIS"
 
+  # 4326 CRS is the WGS default (Mercator)
+  crs <- 4326
+  buffer_size <- 50000 #meter based buffer (5 Km)
+  
 
+# FUNCTIONS ---------------------------------------------------------------
 
-# BOUNDARIES NEEDED -----------------------------------------------------
-
-  
-  # Arrange the bounaries and rasters needed
-  
-  zmb_neighbors <- ne_countries(scale = "large", returnclass = "sf") %>% 
-    filter(sovereignt %in% c("Malawi", "Mozambique", "Botswana", "Namibia",
-      "Democratic Republic of the Congo", "Angola", "United Republic of Tanzania", "Zimbabwe"))
-  
-  ne_geo <- raster(file.path(data, "SR_LR", "SR_LR.tif")) 
-  new_proj <- crs(ne_geo)
-  
-  zmb_admin0 <- ne_countries(scale = "large", returnclass = "sf") %>% 
-    filter(sovereignt == "Zambia") 
-  
-  # Set the map range for centering the map in the ggplot call
-  mapRange <- c(range(st_coordinates(zmb_admin0)[, 1]), range(st_coordinates(zmb_admin0)[, 2]))
+  # Function to return neighbors of a given contry
+  geo_neighbors <- function(country = "Zambia") {
     
-  ne_cities <- st_read(file.path(gis, "ne_10m_populated_places_simple", "ne_10m_populated_places_simple.shp")) %>% 
-    filter(sov0name == "Zambia", pop_max > 1e5)
+    # Arrange the bounaries and rasters needed
+    world <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf") 
+    
+    #stopifnot({{country}} %in% world$sovereignt)
+    if(!{{country}} %in% world$sovereignt) {
+           stop('Country name is not the Natural Earth reference dataset. Please enter a name from dataset.')
+         }
+    
+    focus_country <-  rnaturalearth::ne_countries(scale = "large", returnclass = "sf") %>% 
+      dplyr::filter(sovereignt == {{country}})
+    
+    # Intersect selected country with those in the world to determine neighbors
+    get_neighbors <- world %>% 
+      sf::st_transform(., crs = st_crs(crs)) %>% 
+      dplyr::mutate(neighbors = sf::st_intersects(focus_country, world, sparse = FALSE) %>% t()) %>% 
+      dplyr::filter(neighbors == "TRUE") 
+    
+    return(get_neighbors)
+  }
   
-  # Note that the order for the bbox is xmin, xmax, ymin, ymax not same as st_bbox()
-  bbox <- st_bbox(zmb_admin0)
-  new_bbox <- c(bbox[1] + 1, bbox[3] + 1, bbox[2] - 1 , bbox[4] - 1) 
   
-  zmb_admin1 <- ne_states(country = "Zambia", returnclass = "sf") 
+ # Return admin0 bondary for a given country
+  get_admin0 <- function(country = "Zambia") {
+    
+    admin0 <- 
+      rnaturalearth::ne_countries(scale = "large", returnclass = "sf") %>% 
+      dplyr::filter(sovereignt == {{country}}) %>% 
+      sf::st_transform(., crs = st_crs(crs))
+    
+    return(admin0)    
+  }
   
   
-  zmb_admin0_chop <- st_crop(zmb_admin0, xmin = 21, ymin = -19, xmax = 35, ymax = -8)
-  ne_geo_chop <- crop(ne_geo, zmb_admin0_chop)
-  spdf <- as(ne_geo_chop, "SpatialPixelsDataFrame") %>% as.data.frame(.)
+# Return admin1 boundary for a given country
+  get_admin1 <- function(country = "Zambia") {
+    admin1 <- 
+      rnaturalearth::ne_states(country = {{country}}, returnclass = "sf") %>% 
+      sf::st_transform(., crs = st_crs(crs))
+    
+    return(admin1)  
+  }
+  
+# Return terrain for the country's bounding box, add some 
+  get_terrain <- function(country) {
+    
+    # Grab terrain for a selected area of interest (AOI)
+    
+    #TODO: CHECK IF SR_LR file exists, if not download it with google drive
+    #drive_download(dem_url)
+    
+    ne_geo <- 
+      raster(file.path(gis, "SR_LR", "SR_LR.tif")) 
+    
+    #tmp <- elevatr::get_elev_raster(zmb_admin0, z = 5) 
+    
+    zmb_chop <- st_crop(get_admin0({{country}}), st_bbox(get_admin0({{country}})))
+    spdf <- crop(ne_geo, zmb_chop)  %>% 
+      as(., "SpatialPixelsDataFrame") %>% as.data.frame(.)  
+    
+    return(spdf)
+  }
   
   
+  terrain_map <- function(country = "Zambia") {
+    
+    zmb_admin0 <- get_admin0({{country}})
+    zmb_admin1 <- get_admin1({{country}})
+    zmb_ngbhrs <- geo_neighbors({{country}})
+    
+    # Set the map range for centering the map in the ggplot call
+    mapRange <- c(range(st_coordinates(zmb_admin0)[, 1]), range(st_coordinates(zmb_admin0)[, 2]))
+    
+    spdf <- get_terrain({{country}})
+    
+    p <- ggplot() +
+      geom_tile(data = filter(spdf, SR_LR < 210), aes(x = x, y = y, alpha = SR_LR)) +
+      scale_alpha(name = "", range = c(0.6, 0), guide = F) +
+      theme(legend.position = "none") + 
+      geom_sf(data = zmb_ngbhrs, fill = "#d9d9d9", alpha = 0.35, size = 0.25, colour = grey70k) +
+      #geom_sf_text(data = zmb_neighbors, aes(label = sovereignt), family = "Source Sans Pro" ) +
+      geom_sf(data = zmb_admin0, colour = "white", fill = "grey93", size = 4, alpha = 0.25) +
+      geom_sf(data = zmb_admin0, colour = "black", fill = "NA") +
+      geom_sf(data = zmb_admin1, fill = "NA", linetype = "dotted") +
+      #geom_sf_label_repel(data = ne_cities, aes(label = name), alpha = 0.90, family = "Source Sans Pro Light") +
+      si_style() +
+      coord_sf(xlim = mapRange[c(1:2)], ylim = mapRange[c(3:4)]) +
+      theme_void()
+    
+    return(p)
+  }
+  
+# PLOTS -------------------------------------------------------------------
 
- zmb_base <- 
-   ggplot() +
-    geom_tile(data = filter(spdf, SR_LR < 210), aes(x = x, y = y, alpha = SR_LR)) +
-    scale_alpha(name = "", range = c(0.6, 0), guide = F) +
-    theme(legend.position = "none") + 
-  geom_sf(data = zmb_neighbors, fill = "#d9d9d9", alpha = 0.35, size = 0.25, colour = "#969696") +
-  #geom_sf_text(data = zmb_neighbors, aes(label = sovereignt), family = "Source Sans Pro" ) +
-    geom_sf(data = zmb_admin0, colour = "white", fill = "grey93", size = 4, alpha = 0.25) +
-    geom_sf(data = zmb_admin0, colour = "black", fill = "NA") +
-  geom_sf(data = zmb_admin1, fill = "NA") +
-   geom_sf_label_repel(data = ne_cities, aes(label = name), alpha = 0.90, family = "Source Sans Pro Light") +
-  si_style() +
-  coord_sf(xlim = mapRange[c(1:2)], ylim = mapRange[c(3:4)]) +
-  theme_void()
+ terrain_map("Uganda")
+ terrain_map("United Republic of Tanzania")
+ terrain_map("Democratic Republic of the Congo")
+ terrain_map("Malawi")
+
+
    
     

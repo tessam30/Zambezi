7d63d9b38059fae25f8904890b147012bf2c546c
Tim Essam <tim.essam@gmail.com>
2021-02-04 22:34:19 -0500

    add prep_new disag plot

--- Scripts/COP21_ZMB_PrEP_NEW_Map_request.R
+++ Scripts/COP21_ZMB_PrEP_NEW_Map_request.R
@@ -79,44 +79,77 @@
   
   
   # Custom request for PrEP 
-  disagg_regroup_m <- c("25-29, Male","30-34, Male", "35-39, Male",
-                        "40-44, Male", "45-49, Male", 
-                        "50+, Male")
+  disagg_regroup_m <- c("Age/Sex_Male_25-29", "Age/Sex_Male_30-34",
+                        "Age/Sex_Male_35-39", "Age/Sex_Male_40-44",
+                        "Age/Sex_Male_45-49", "Age/Sex_Male_50+")
   
-  disagg_regroup_f <- c("25-29, Female", "30-34, Female", "35-39, Female",
-                        "40-44, Female", "45-49, Female", 
-                        "50+, Female")
+  disagg_regroup_f <- c("Age/Sex_Female_25-29", "Age/Sex_Female_30-34",
+                        "Age/Sex_Female_35-39", "Age/Sex_Female_40-44",
+                        "Age/Sex_Female_45-49", "Age/Sex_Female_50+")
 
-  prep_new_disag <- 
+  # Pull out standard disaggs; KeyPop pulled out separately -- they will distort targets and totals
+  prep_new_disag_kp <- 
     genie %>% 
-    filter(str_detect(categoryoptioncomboname, ("PrEP|People|PWIG|TG|PWI"), negate = T),
-           indicator == "PrEP_NEW") %>% 
-    rename(cc = categoryoptioncomboname) %>% 
-    mutate(disags = case_when(
-      cc == "15-19, Female" ~ "F 15-19",
-      cc == "15-19, Male" ~ "M 15-19",
-      cc == "20-24, Female" ~ "F 20-24",
-      cc == "20-24, Male" ~ "M 20-24",
-      cc %in% disagg_regroup_m ~ "M 25-50+",
-      cc %in% disagg_regroup_f ~ "F 25-50+",
-      TRUE ~ cc)
-      ) %>%
-      mutate(disags = fct_relevel(disags,
-                           "M 15-19", 
-                           "F 15-19",
-                           "M 20-24",
-                           "F 20-24",
-                           "M 25-50+",
-                           "F 25-50+",
-                           "MSM",
-                           "FSW"))%>% 
-    filter(!disags %in% c("default")) %>% 
-    group_by(disags, period, period_type) %>% 
+      filter(indicator == "PrEP_NEW",
+             fundingagency == "USAID",
+             standardizeddisaggregate %in% c("KeyPopAbr")) %>% 
+      unite(combo, c(standardizeddisaggregate, otherdisaggregate), remove = F) %>%
+      filter(str_detect(combo, "(FSW|MSM)")) %>% 
+      mutate(disags = case_when(
+        combo == "KeyPopAbr_FSW" ~ "FSW",
+        combo == "KeyPopAbr_MSM" ~ "MSM",
+        TRUE ~ NA_character_
+      ), 
+      core_disag = 0) %>% 
+    mutate(core_disag = 0) %>% 
+    group_by(disags, period, period_type, core_disag) %>% 
     summarise(total = sum(val, na.rm = TRUE)) %>% 
     ungroup() %>% 
     unite(., "period_combo", c(period, period_type), remove = F) %>% 
     filter(!period_combo %in% c("FY21Q1_results", "FY20Q2_results", "FY20Q4_results"))
   
+
+  prep_new_disag <- 
+    genie %>% 
+    filter(indicator == "PrEP_NEW",
+           fundingagency == "USAID",
+           standardizeddisaggregate %in% c("Age/Sex")) %>% 
+    unite(combo, c(standardizeddisaggregate, sex, ageasentered), remove = F) %>%
+      mutate(disags = case_when(
+      combo == "Age/Sex_Female_15-19" ~ "F 15-19",
+      combo == "Age/Sex_Male_15-19" ~ "M 15-19",
+      combo == "Age/Sex_Female_20-24" ~ "F 20-24",
+      combo == "Age/Sex_Male_20-24" ~ "M 20-24",
+      combo %in% disagg_regroup_m ~ "M 25-50+",
+      combo %in% disagg_regroup_f ~ "F 25-50+",
+      TRUE ~ combo)
+      ) %>% 
+    mutate(core_disag = 1) %>% 
+    group_by(disags, period, period_type, core_disag) %>% 
+    summarise(total = sum(val, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    unite(., "period_combo", c(period, period_type), remove = F) %>% 
+    filter(!period_combo %in% c("FY21Q1_results", "FY20Q2_results", "FY20Q4_results"))
+  
+  prep_new_disag_all <- 
+    prep_new_disag %>% 
+    bind_rows(prep_new_disag_kp) %>% 
+    mutate(disags = fct_relevel(disags,
+                            "M 15-19", 
+                            "F 15-19",
+                            "M 20-24",
+                            "F 20-24",
+                            "M 25-50+",
+                            "F 25-50+",
+                            "FSW",
+                            "MSM")
+           )
+
+
+  prep_new_disag %>% 
+    group_by(period, period_type) %>% 
+    summarise(total = sum(total, na.rm = TRUE))
+
   
     
     
@@ -175,13 +208,13 @@
   # Hi Tim, could you replicate this with prep curr but streamline with achievements and targets in the same bar? 
   # Q2 fy20 results against targets one bar , Apr fy20 results against targets another bar, q1 results against targets 
   # a third bar? Same age sex disaggs?  
-  max <- max(prep_new_disag$total)
+  max <- max(prep_new_disag_all$total)
   
-  prep_new_disag %>% 
+  prep_new_disag_all %>% 
     select(-period_combo) %>% 
     spread(period_type, total) %>% 
     mutate(Achievement = cumulative / targets,
-           dotted_line = if_else(Achievement > 1, "white", grey90k)) %>% 
+           dotted_line = if_else(Achievement > 1, "white", trolley_grey_light)) %>% 
     ggplot(aes(x = period)) + 
       geom_col(aes(y = targets), fill = grey10k) +
       geom_col(aes(y = cumulative), 
@@ -199,19 +232,32 @@
                   linetype = "dashed") +
       si_style_xline() +
       scale_y_continuous(labels = comma, expand = c(.01, 0),
-                         limits = c(0, max)) +
+                         limits = c(0, max+ 1000)) +
     facet_wrap(~disags, nrow = 1, strip.position = "bottom") +
     scale_color_identity() +
     theme(
       strip.placement = "outside",
       strip.text = element_text(hjust = 0.5),
       axis.text.y = element_blank(),
+      plot.title = element_markdown(family = "Source Sans Pro Regular"),
+      plot.subtitle = element_markdown(size = 11, family = "Source Sans Pro"),
       text = element_text(family = "Source Sans Pro")
     ) +
-    labs(x = NULL, y = NULL, title = "PrEP_NEW RESULTS AND TARGETS BY AGE BANDS")
+    labs(x = NULL, y = NULL, title = "PrEP_NEW <span style = 'color:#287c6f;'>**RESULTS**</span> AND 
+         <span style = 'color:#808080;'>**TARGETS**</span> BY AGE BANDS FOR USAID",
+         caption = "Source: Genie pull as of 2020-02-04")
+         # subtitle = "<span style = 'color:#287c6f;'>**Results in green**</span><span style = 'color:#808080;'>
+         # <br>Targets in grey</span style>")
+  
+  si_save(here(images, "ZMB_PrEP_NEW_disaggs_summary.png"),
+          scale = 1.25)
     
     
-    
+    prep_new_disag %>% 
+      group_by(period, period_type) %>% 
+      summarise(total = sum(total, na.rm = TRUE))
+      
+      
 
   
   
